2bfaaac7f7b51462a6a3e9a9e9029c27ce0874ba
ghsyafii <73457694+ghsyafii@users.noreply.github.com>
2021-02-08 12:43:03 +0800

    installed and implemented morgan middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 //express app
 const app = express();
@@ -11,24 +12,41 @@ app.set('view engine', 'ejs');
 
 app.listen(3000);
 
+//middleware to log get requests
+
+// app.use((req, res, next) => {
+//     console.log('new request made:');
+//     console.log('host: ', req.hostname);
+//     console.log('path: ', req.path);
+//     console.log('method', req.method);
+//     next();
+// });
+
+//morgan middleware
+
+app.use(morgan('tiny'));
+
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
-    // res.send('<h1>Yoooo</h1>');
+    const blogs = [
+        {title: 'The Land Before Time', snippet: 'This is the deal, Holyfield.'},
+        {title: 'The Land Before Space', snippet: 'This is the deal, Tyson.'},
+        {title: 'The Land Before Nothing', snippet: 'This is the deal, Douglas.'},
+        {title: 'The Land Before Matter', snippet: 'This is the deal, Ziggurat.'},
+        {title: 'The Land Before Peanut Butter', snippet: 'This is the deal, Tan.'}
+    ];
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
-    // res.send('<h1>About</h1>');
+    res.render('about', { title: 'About' });
 });
 
-//redirects
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create' });
+})
 
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
-
-//middleware - default if the others do not fire
+//middleware 404 - default if the others do not fire
 
 app.use((req, res) => {
-    res.status(404).sendFile('/views/404.html', { root: __dirname })
+    res.status(404).render('404', { title: '404' });
 })
