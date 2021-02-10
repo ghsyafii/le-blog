db6451583bec0dbf589e136260823b84bb1e06d1
ghsyafii <73457694+ghsyafii@users.noreply.github.com>
2021-02-10 13:00:51 +0800

    prepare to upload to heroku

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -8,11 +8,47 @@ const blog_index = (req, res) => {
     Blog.find().sort( { createdAt: -1 })
         .then((result) => {
             //render to this route ie /blogs the index.ejs file and pass the title, and for the blogs, pass the result - refer to index html to see the relationships
-            res.render('index', { title: 'All Blogs', blogs: result});
+            res.render('blogs/index', { title: 'All Blogs', blogs: result});
         })
         .catch((error) => {
             console.log(error)
         })
 }
 
-module.exports = { blog_index }
+const blog_details = (req,res) => {
+    //get id from req object (the last bit ".id in this case" corresponds to whatever comes after :)
+    const id = req.params.id;
+    Blog.findById(id)
+        .then(result => {
+            res.render('blogs/details', { blog : result, title: 'Blog Details' });
+        })
+        .catch(err => {
+            res.status(404).render('404', { title: 'Blog not found' })
+            })
+}
+
+const blog_create_get = (req,res) => {
+    res.render('blogs/create', { title: 'Create' });
+}
+
+const blog_create_post = (req,res) => {
+    const blog = new Blog(req.body);
+    blog.save()
+        .then(result => {
+            //redirect to homepage that shows all blogs
+            res.redirect('/blogs');
+        })
+        .catch(err => console.log(err))
+}
+
+const blog_delete = (req,res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/blogs' })
+        })
+        .catch(err => console.log(err))
+}
+
+//export above function so you have access to anywhere you import it
+module.exports = { blog_index, blog_details, blog_create_get, blog_create_post, blog_delete }
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,59 +1,22 @@
 const express = require('express');
-const Blog = require('../models/blog');
 //create new express router - which is like a mini app
 const router = express.Router();
+//import blog_index
+const blogController = require('../controllers/blogController');
 
 //blog routes
-router.get('/', (req,res) => {
-    //.find() finds all stuff in collection
-    //.sort() sorts it by some standard, same -1 +1 rule as JS
-    Blog.find().sort( { createdAt: -1 })
-        .then((result) => {
-            //render to this route ie /blogs the index.ejs file and pass the title, and for the blogs, pass the result - refer to index html to see the relationships
-            res.render('index', { title: 'All Blogs', blogs: result});
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-});
+router.get('/', blogController.blog_index);
 
-//POST request: save new blog entry to database
+router.post('/', blogController.blog_create_post);
 
-router.post('/', (req, res) => {
-    const blog = new Blog(req.body);
-    blog.save()
-        .then(result => {
-            //redirect to homepage that shows all blogs
-            res.redirect('/blogs');
-        })
-        .catch(err => console.log(err))
-});
-
-router.get('/create', (req, res) => {
-    res.render('create', { title: 'Create' });
-});
+router.get('/create', blogController.blog_create_get);
 
 //GET: Post by ID (include : in front of route parameter
 
-router.get('/:id', (req,res) => {
-    //get id from req object (the last bit ".id in this case" corresponds to whatever comes after :)
-    const id = req.params.id;
-    Blog.findById(id)
-        .then(result => {
-            res.render('details', { blog : result, title: 'Blog Details' });
-        })
-        .catch(err => console.log(err))
-});
+router.get('/:id', blogController.blog_details);
 
 //delete
-router.delete('/:id', (req,res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({ redirect: '/blogs' })
-        })
-        .catch(err => console.log(err))
-})
+router.delete('/:id', blogController.blog_delete)
 
 //export router
 
